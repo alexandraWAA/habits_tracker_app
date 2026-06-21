daf9e1cef319e2e14191f6bacc261e382c897ed9
Aleksandra Volchikhina <aleksandravolcihina@gmail.com>
2026-06-21 20:54:54 +0300

    1st commit

---
+++ lms/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from lms.models import Course, Lesson, Subscription, Payment
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'owner', 'created_at']
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'description']
+    list_filter = ['created_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'course', 'owner', 'created_at']
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'description']
+    list_filter = ['course', 'created_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'course', 'created_at']
+    list_display_links = ['id']
+    list_filter = ['course', 'created_at']
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'course', 'amount', 'status', 'created_at']
+    list_display_links = ['id']
+    list_filter = ['status', 'created_at']
+    search_fields = ['user__email', 'course__name']
