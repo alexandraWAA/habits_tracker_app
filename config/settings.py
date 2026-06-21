6cfe32b211ddf0d355be6451fc12f24f6555f8cd
Aleksandra Volchikhina <aleksandravolcihina@gmail.com>
2026-06-21 21:12:50 +0300

    3st commit

--- config/settings.py
+++ config/settings.py
@@ -65,12 +65,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'habits_db'),
-        'USER': os.getenv('DB_USER', 'habits_user'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
